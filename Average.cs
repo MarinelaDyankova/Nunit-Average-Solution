3806ead60998a939e12154723aafe8ed57fcb8d6
MarinelaDyankova <marinela.dyankova@gmail.com>
2022-05-08 23:12:11 +0300

    Initial_Commit

---
+++ Average.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Average
+{
+    public class Average
+    {
+        public static double AverageNumbers(double[] arr)
+        {
+            double sum = 0;
+           
+            double average = 0;
+            if (arr.Length != 0) {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                   
+                }
+                average = sum / arr.Length;
+            } else if (arr.Length == 0)
+            {
+                average = 0;    
+            }
+
+            return average;
+        }
+        static void Main()
+        {
+            double result = AverageNumbers(new double[] { 5, 8 });
+            Console.WriteLine($"{result:f2}");
+        }
+    }
+
+    }
+
